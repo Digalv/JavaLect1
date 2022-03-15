62ae78042bd4494bd626d7d05009b21963638317
Digalv <daniil.tabakov004@gmail.com>
2022-03-15 17:37:36 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,526 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String YELLOW = "\u001B[33m";
+        String GREEN = "\u001B[32m";
+        String BLACK = "\u001B[30m";
+        String WHITE = "\u001B[37m";
+        String RED = "\u001B[31m";
+        char Sim = '\u2587';
+        int end = 5;
+        /**
+         * First line
+         */
+        for(int i = 0; i <= 30; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(' ');
+        /**
+         * Second line
+         */
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 9; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= end; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 3th line
+         */
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 7; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(RED + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 4th line
+         */
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 6; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 6; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(RED + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 5th line
+         */
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 6; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 6; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(RED + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 1 ; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 6h line
+         */
+        System.out.printf(WHITE + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 9; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(RED + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end - 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 7th line
+         */
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(WHITE + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(RED + Sim);
+        }
+        System.out.printf((YELLOW + Sim) + (RED + Sim) + (BLACK + Sim));
+        for(int i = 0; i <= end - 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 8th line
+         */
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(RED + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(RED + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end - 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 9th line
+         */
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(YELLOW + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(RED + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end - 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 10th line
+         */
+        System.out.printf(WHITE + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= end; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 11th
+         */
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 11; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(YELLOW + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 2; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 12th
+         */
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 2; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 13th
+         */
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(GREEN + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 7; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 14th
+         */
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(GREEN + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(YELLOW + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 5; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 15th
+         */
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(WHITE + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(GREEN + Sim);
+        }
+        for(int i = 0; i <= 6; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= end + 6; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 16th
+         */
+        for(int i = 0; i <= 4; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(GREEN + Sim);
+        }
+        for(int i = 0; i <= 3; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= end + 7; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 17th
+         */
+        for(int i = 0; i <= 7; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= 2; i++)
+        {
+            System.out.printf(YELLOW + Sim);
+        }
+        for(int i = 0; i <= 1; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= end + 9; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 18th
+         */
+        for(int i = 0; i <= 8; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.printf(BLACK + Sim);
+        System.out.printf(WHITE + Sim);
+        System.out.printf(WHITE + Sim);
+        System.out.printf(YELLOW + Sim);
+        System.out.printf(WHITE + Sim);
+        System.out.printf(WHITE + Sim);
+        System.out.printf(BLACK + Sim);
+        for(int i = 0; i <= end + 9; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 19th
+         */
+        for(int i = 0; i <= 9; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.printf(BLACK + Sim);
+        }
+        for(int i = 0; i <= end + 9; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        /**
+         * 20-21th
+         */
+        for(int i = 0; i <= 30; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+        for(int i = 0; i <= 30; i++)
+        {
+            System.out.printf(WHITE + Sim);
+        }
+        System.out.println(" ");
+
+    }
+
+}
